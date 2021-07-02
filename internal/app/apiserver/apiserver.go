161ed2a970ef4136f0663c432a220c5906c8383a
Sereggan <sergei.ni@yandex.ru>
2021-07-03 01:22:25 +0300

    db draft docker migrations tests

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -30,6 +30,10 @@ func (s *APIServer) Start() error {
 
 	s.configureRouter()
 
+	if err := s.configureStore(); err != nil {
+		return err
+	}
+
 	s.logger.Info("starting API server")
 
 	return http.ListenAndServe(s.config.BindAddr, s.router)
@@ -56,4 +60,14 @@ func (s *APIServer) handleHello() http.HandlerFunc {
 	return func(w http.ResponseWriter, e *http.Request) {
 		io.WriteString(w, "Hello")
 	}
+}
+
+func (s *APIServer) configureStore() error {
+	st := store.New(s.config.Store)
+	if err := st.Open(); err != nil {
+		return err
+	}
+	s.store = st
+
+	return nil
 }
---
+++ internal/app/store/userrepository_test.go
@@ -0,0 +1,30 @@
+package store_test
+
+import (
+	"github.com/Sereggan/simpleWebApp/internal/app/model"
+	"github.com/Sereggan/simpleWebApp/internal/app/store"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestUserRepository_Create(t *testing.T) {
+	s, teardown := store.TestStore(t, databaseURL)
+	defer teardown("users")
+
+	u, err := s.User().Create(&model.User{
+		Email: "exampleEmail@org",
+	})
+
+	assert.NoError(t, err)
+	assert.NotNil(t, u)
+}
+
+func TestUserRepository_FindByEmail(t *testing.T) {
+	s, teardown := store.TestStore(t, databaseURL)
+	defer teardown("users")
+
+	email := "user@exampe.org"
+	_, err := s.User().FindByEmail(email)
+
+	assert.Error(t, err)
+}
